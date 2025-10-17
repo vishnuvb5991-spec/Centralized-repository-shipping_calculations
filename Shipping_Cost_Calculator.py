ffa97416506557c0d021a14e7a21b69899e440a2
vishnuvb5991-spec <vishnuvb5991@gmail.com>
2025-10-17 08:14:28 -0400

    added additional line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,5 +10,5 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
- # Here is a new update by vishnuvb5991-spec
+ # Here is another update by vishnuvb5991-spec
 
